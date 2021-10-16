ac224e091aa622749f0a60950c65ae23c15cd760
YotamZiv298 <yotamziv87@gmail.com>
2021-10-16 19:11:09 +0300

    Updated look

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,13 +1,19 @@
 package main;
 
+import javax.swing.JFrame;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 public class Main {
 
     public static final int FRAME_WIDTH = 500;
-    public static final int FRAME_HEIGHT = 720;
+    public static final int FRAME_HEIGHT = 520;
 
-    public static void main(String[] args) {
-        FlappyBirdMultiplayer flappyBirdMultiplayer = new FlappyBirdMultiplayer();
+    public static void main(String[] args) throws UnsupportedLookAndFeelException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        JFrame.setDefaultLookAndFeelDecorated(true);
 
+        new FlappyBirdMultiplayer();
     }
 
 }
