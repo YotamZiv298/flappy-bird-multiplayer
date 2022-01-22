2b7d15f30feba48dbb09cc78e56b12bbe153ef61
YotamZiv298 <yotamziv87@gmail.com>
2022-01-22 20:49:11 +0200

    Using globals instead of save button

--- src/state/settings/tabs/NetworkTab.java
+++ src/state/settings/tabs/NetworkTab.java
@@ -1,15 +1,13 @@
 package state.settings.tabs;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
 import java.util.ArrayList;
 
 import javax.swing.BorderFactory;
-import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 import main.Main;
+import main.resources.Globals;
 
 public class NetworkTab extends JPanel {
 
@@ -18,24 +16,12 @@ public class NetworkTab extends JPanel {
 
     protected ArrayList<JTextField> _fields;
 
-
-    protected JButton _saveButton;
-
-    protected ArrayList<JButton> _buttons;
-
     public NetworkTab() {
-        String address = null;
-        try {
-            address = InetAddress.getLocalHost().getHostAddress();
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
-        }
-
-        _addressField = new JTextField(address);
+        _addressField = new JTextField(Globals.getProperty(Globals.IP_ADDRESS));
         _addressField.setBounds(Main.FRAME_WIDTH / 2 - Main.FRAME_WIDTH / 6, Main.FRAME_HEIGHT / 2 + 2 * 40, 150, 40);
         _addressField.setColumns(11);
 
-        _portField = new JTextField("0");
+        _portField = new JTextField(Globals.getProperty(Globals.PORT));
         _portField.setBounds(Main.FRAME_WIDTH / 2 - Main.FRAME_WIDTH / 6, Main.FRAME_HEIGHT / 2 + 4 * 40, 150, 40);
         _portField.setColumns(4);
 
@@ -46,16 +32,6 @@ public class NetworkTab extends JPanel {
         for (JTextField field : _fields) {
             field.setFocusable(true);
             add(field);
-        }
-
-        _saveButton = new JButton("Save");
-
-        _buttons = new ArrayList<>();
-        _buttons.add(_saveButton);
-
-        for (JButton button : _buttons) {
-            button.setFocusable(false);
-            add(button);
         }
 
         setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), "Network"));
