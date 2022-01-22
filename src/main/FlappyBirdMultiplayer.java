fb8b816f32793dc4f9a6cbbe611081a9d8393b7b
YotamZiv298 <yotamziv87@gmail.com>
2022-01-22 20:55:09 +0200

    Added client and server connection to game

--- src/main/FlappyBirdMultiplayer.java
+++ src/main/FlappyBirdMultiplayer.java
@@ -1,12 +1,17 @@
 package main;
 
+import java.awt.BorderLayout;
 import java.awt.CardLayout;
 import java.awt.Dimension;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+import client.Client;
+import client.framework.ClientListener;
+import framework.ConnectionDisplay;
 import framework.Keyboard;
+import main.resources.Globals;
 import state.MainMenu;
 
 public class FlappyBirdMultiplayer extends JFrame {
@@ -14,10 +19,9 @@ public class FlappyBirdMultiplayer extends JFrame {
     public static CardLayout appCardLayout;
     public static JPanel appPanelContainer;
 
-    private MainMenu _mainMenu;
-//    private GamePanel _gamePanel;
-
-//    protected ArrayList<JPanel> _panels;
+    public static Client client;
+    private ClientListener _clientListener = new ClientListener();
+    public static ConnectionDisplay connectionDisplay;
 
     public FlappyBirdMultiplayer() {
         super("Flappy Bird Multiplayer");
@@ -25,25 +29,19 @@ public class FlappyBirdMultiplayer extends JFrame {
         appCardLayout = new CardLayout();
         appPanelContainer = new JPanel(appCardLayout);
 
-//        _mainMenu = new MainMenu();
-//        _gamePanel = new GamePanel();
+        client = new Client(Globals.getProperty(Globals.IP_ADDRESS), Integer.parseInt(Globals.getProperty(Globals.PORT)), _clientListener);
+
+        connectionDisplay = new ConnectionDisplay();
+        connectionDisplay.setPreferredSize(new Dimension(0, 10));
 
         appPanelContainer.add(new MainMenu(), MainMenu.class.getSimpleName());
-//        appPanelContainer.add(_gamePanel, GamePanel.class.getSimpleName());
 
         appCardLayout.show(appPanelContainer, MainMenu.class.getSimpleName());
 
-//        _panels = new ArrayList<>();
-//        _panels.add(_mainMenu);
-//        _panels.add(_gamePanel);
+        add(connectionDisplay, BorderLayout.PAGE_START);
+        add(appPanelContainer, BorderLayout.CENTER);
 
-
-//        for (JPanel panel : _panels) {
-//            add(panel);
-//        }
-
-        add(appPanelContainer);
-
+        setFocusable(true);
         addKeyListener(Keyboard.getInstance());
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setPreferredSize(new Dimension(Main.FRAME_WIDTH, Main.FRAME_HEIGHT));
