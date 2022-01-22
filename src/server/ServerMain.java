abca9c883fd45363b68eb998bfb58145ef92b96e
YotamZiv298 <yotamziv87@gmail.com>
2022-01-22 21:21:04 +0200

    Change UI

--- src/server/ServerMain.java
+++ src/server/ServerMain.java
@@ -1,8 +1,14 @@
 package server;
 
+import javax.swing.JFrame;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 public class ServerMain {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnsupportedLookAndFeelException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        JFrame.setDefaultLookAndFeelDecorated(true);
         new ServerFrame();
     }
 
