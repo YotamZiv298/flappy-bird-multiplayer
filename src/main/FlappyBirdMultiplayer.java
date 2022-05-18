17a4068aca19a9d305812784c65bfa541fef5346
YotamZiv298 <yotamziv87@gmail.com>
2022-05-18 14:48:54 +0300

    Added constants and background

--- src/main/FlappyBirdMultiplayer.java
+++ src/main/FlappyBirdMultiplayer.java
@@ -1,27 +1,33 @@
 package main;
 
-import java.awt.BorderLayout;
-import java.awt.CardLayout;
-import java.awt.Dimension;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-
 import client.Client;
 import client.framework.ClientListener;
 import framework.ConnectionDisplay;
 import framework.Keyboard;
 import main.resources.Globals;
+import main.resources.OSDetector;
 import state.MainMenu;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import java.awt.CardLayout;
+import java.awt.Dimension;
+import java.util.Random;
 
 public class FlappyBirdMultiplayer extends JFrame {
 
+    private static final Random random = new Random();
     public static CardLayout appCardLayout;
     public static JPanel appPanelContainer;
 
     public static Client client;
-    private ClientListener _clientListener = new ClientListener();
     public static ConnectionDisplay connectionDisplay;
+
+    public static String playerName = "Player" + String.format("%06d", random.nextInt(999999));
+
+    public static final String GAME_LOGO = OSDetector.isWindows() ? "src\\images\\bird.png" : "src/images/bird.png";
 
     public FlappyBirdMultiplayer() {
         super("Flappy Bird Multiplayer");
@@ -29,7 +35,8 @@ public class FlappyBirdMultiplayer extends JFrame {
         appCardLayout = new CardLayout();
         appPanelContainer = new JPanel(appCardLayout);
 
-        client = new Client(Globals.getProperty(Globals.IP_ADDRESS), Integer.parseInt(Globals.getProperty(Globals.PORT)), _clientListener);
+        ClientListener clientListener = new ClientListener();
+        client = new Client(Globals.getProperty(Globals.IP_ADDRESS), Integer.parseInt(Globals.getProperty(Globals.PORT)), clientListener);
 
         connectionDisplay = new ConnectionDisplay();
         connectionDisplay.setPreferredSize(new Dimension(0, 10));
@@ -41,6 +48,7 @@ public class FlappyBirdMultiplayer extends JFrame {
         add(connectionDisplay, BorderLayout.PAGE_START);
         add(appPanelContainer, BorderLayout.CENTER);
 
+        setIconImage(new ImageIcon(GAME_LOGO).getImage());
         setFocusable(true);
         addKeyListener(Keyboard.getInstance());
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
