5e66c0ad3e5dba458099b450a13bd141942a902f
YotamZiv298 <yotamziv87@gmail.com>
2022-05-27 12:54:38 +0300

    Removed database session table

--- src/database/PostgreSQLJDBC.java
+++ src/database/PostgreSQLJDBC.java
@@ -1,7 +1,6 @@
 package database;
 
 import database.tables.leaderboard.LeaderboardJDBC;
-import database.tables.session.SessionJDBC;
 
 import java.sql.SQLException;
 
@@ -13,20 +12,14 @@ public class PostgreSQLJDBC {
     private static final String _user = "postgres";
     private static final String _password = "root";
 
-    private static SessionJDBC _sessionJDBC;
     private static LeaderboardJDBC _leaderboardJDBC;
 
     static {
         try {
-            _sessionJDBC = new SessionJDBC(_url, _user, _password);
             _leaderboardJDBC = new LeaderboardJDBC(_url, _user, _password);
         } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
         }
-    }
-
-    public static SessionJDBC sessionJDBCInstance() {
-        return _sessionJDBC;
     }
 
     public static LeaderboardJDBC leaderboardJDBCInstance() {
--- src/server/framework/ServerListener.java
+++ src/server/framework/ServerListener.java
@@ -3,13 +3,11 @@ package server.framework;
 import client.ClientInstance;
 import database.PostgreSQLJDBC;
 import database.tables.leaderboard.PlayerData;
-import database.tables.session.Session;
 import server.ServerFrame;
 
 import java.io.ObjectOutputStream;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Random;
 
 public class ServerListener implements ServerInterface {
 
@@ -29,11 +27,12 @@ public class ServerListener implements ServerInterface {
 
     @Override
     public Message<?> received(ClientInstance client, Message<?> message) {
-        String format = "\n" +
-                "~~~~~~~~~~~~~~~~~\n" +
-                "%s request :: code %s\n" +
-                "data: %s\n" +
-                "~~~~~~~~~~~~~~~~~";
+        String format = """
+
+                ~~~~~~~~~~~~~~~~~
+                %s request :: code %s
+                data: %s
+                ~~~~~~~~~~~~~~~~~""";
         String log = String.format(format, client.toString(), message.getRequestCode(), message.getData());
 
         ServerFrame.addLog(log);
@@ -45,133 +44,38 @@ public class ServerListener implements ServerInterface {
     public Message<?> respond(ClientInstance client, Message<?> message) {
         try {
             Message<?> response = processReceivedData(client, message);
-            String format = "\n" +
-                    "~~~~~~~~~~~~~~~~~\n" +
-                    "Server response :: code %s\n" +
-                    "data: %s\n" +
-                    "~~~~~~~~~~~~~~~~~";
+            String format = """
+
+                    ~~~~~~~~~~~~~~~~~
+                    Server response :: code %s
+                    data: %s
+                    ~~~~~~~~~~~~~~~~~""";
             String log = String.format(format, message.getRequestCode(), response != null ? response.getData() : "null");
 
             ServerFrame.addLog(log);
 
             return response;
-        } catch (SQLException | ClassNotFoundException e) {
+        } catch (SQLException e) {
             e.printStackTrace();
         }
 
         return null;
     }
 
-    public synchronized Message<?> processReceivedData(ClientInstance client, Message<?> message) throws SQLException, ClassNotFoundException {
+    public synchronized Message<?> processReceivedData(ClientInstance client, Message<?> message) throws SQLException {
         Message.RequestCode code = message.getRequestCode();
         Object data = message.getData();
 
-        switch (code) {
-            case NEW_GAME:
-                return createNewGame(client);
-            case JOIN_GAME:
-                return joinGame(client, data);
-            case LEAVE_GAME:
-                return leaveGame(client, data);
-            case DELETE_GAME:
-                return deleteGame(data);
-            case START_GAME:
-                return startGame();
-            case RECEIVE_PLAYERS_IN_SESSION:
-                return getPlayersInSession(data);
-            case IS_SESSION_ACTIVE:
-                return isSessionActive(data);
-            case RECEIVE_LEADERBOARD:
-                return getLeaderboard();
-            case SET_PLAYER_SCORE:
-                return setPlayerScore(data);
-//            case FIRST_CONNECTION_CHECK:
-//                return new Message<>(Message.RequestCode.FIRST_CONNECTION_CHECK, true);
-        }
-
-        return null;
-    }
-
-    public Message<String> createNewGame(ClientInstance client) throws SQLException {
-        Random random = new Random();
-
-        String gameCode;
-        do {
-            gameCode = String.format("%06d", random.nextInt(999999));
-        } while (PostgreSQLJDBC.sessionJDBCInstance().getSession(gameCode) != null);
-
-        Session session = new Session();
-
-        session.setCode(gameCode);
-
-        session.setPlayers(new ArrayList<>());
-        session.addPlayer(client.toString());
-
-        session.setLeaderboard(new ArrayList<>());
-
-        PostgreSQLJDBC.sessionJDBCInstance().addSession(session);
-
-        return new Message<>(Message.RequestCode.NEW_GAME, gameCode);
-    }
-
-    public Message<Boolean> joinGame(ClientInstance client, Object data) throws SQLException {
-        String sessionCode = (String) data;
-//        String[] array = (String[]) data;
-//        String gameCode = array[0];
-//        String player = array[1];
-
-        Session session = PostgreSQLJDBC.sessionJDBCInstance().getSession(sessionCode);
-
-        if (session == null) return new Message<>(Message.RequestCode.JOIN_GAME, Boolean.FALSE);
-
-        session.addPlayer(client.toString());
-
-        Boolean success = PostgreSQLJDBC.sessionJDBCInstance().updateSession(session);
-
-        return new Message<>(Message.RequestCode.JOIN_GAME, success);
-    }
-
-    public Message<Boolean> leaveGame(ClientInstance client, Object data) throws SQLException {
-        String sessionCode = (String) data;
-        Session session = PostgreSQLJDBC.sessionJDBCInstance().getSession(sessionCode);
-
-        if (session == null)
-            return new Message<>(Message.RequestCode.LEAVE_GAME, Boolean.FALSE);
-
-        session.removePlayer(client.toString());
-
-        Boolean success = session.getPlayers().size() != 0 ? Boolean.valueOf(PostgreSQLJDBC.sessionJDBCInstance().updateSession(session)) : deleteGame(sessionCode).getData();
-
-        return new Message<>(Message.RequestCode.LEAVE_GAME, success);
-    }
-
-    public Message<Boolean> deleteGame(Object data) throws SQLException {
-        String sessionCode = (String) data;
-        Session session = PostgreSQLJDBC.sessionJDBCInstance().getSession(sessionCode);
-
-        if (session == null) return new Message<>(Message.RequestCode.DELETE_GAME, Boolean.FALSE);
-
-        PostgreSQLJDBC.sessionJDBCInstance().removeSession(session);
-
-        return new Message<>(Message.RequestCode.DELETE_GAME, Boolean.TRUE);
+        return switch (code) {
+            case START_GAME -> startGame();
+            case RECEIVE_LEADERBOARD -> getLeaderboard();
+            case SET_PLAYER_SCORE -> setPlayerScore(data);
+            default -> null;
+        };
     }
 
     public Message<Boolean> startGame() {
         return new Message<>(Message.RequestCode.START_GAME, Boolean.TRUE);
-    }
-
-    public Message<ArrayList<String>> getPlayersInSession(Object data) throws SQLException {
-        String sessionCode = (String) data;
-        Session session = PostgreSQLJDBC.sessionJDBCInstance().getSession(sessionCode);
-
-        return new Message<>(Message.RequestCode.RECEIVE_PLAYERS_IN_SESSION, session != null ? session.getPlayers() : new ArrayList<>());
-    }
-
-    public Message<Boolean> isSessionActive(Object data) throws SQLException {
-        String sessionCode = (String) data;
-        Session session = PostgreSQLJDBC.sessionJDBCInstance().getSession(sessionCode);
-
-        return new Message<>(Message.RequestCode.IS_SESSION_ACTIVE, session != null ? Boolean.TRUE : Boolean.FALSE);
     }
 
     public Message<ArrayList<PlayerData>> getLeaderboard() throws SQLException {
